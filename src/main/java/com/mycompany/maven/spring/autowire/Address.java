327b93b38d0ef7c24864817dfeac3b1acae5e93d
fjcorona <fjavier.corona@hotmail.com>
2017-11-29 12:02:58 -0600

    Address POJO

---
+++ src/main/java/com/mycompany/maven/spring/autowire/Address.java
@@ -0,0 +1,37 @@
+package com.mycompany.maven.spring.autowire;
+
+public class Address {
+
+    private String street;
+    private String city;
+    private String state;
+
+    public Address() {
+
+    }
+
+    public String getStreet() {
+        return this.street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return this.state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+}
