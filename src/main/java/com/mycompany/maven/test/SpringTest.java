1aba4ef1243f83b4cee1659f8635fcedbcaaf46c
fjcorona <fjavier.corona@hotmail.com>
2017-11-29 11:32:28 -0600

    update sping test

--- src/main/java/com/mycompany/maven/test/SpringTest.java
+++ src/main/java/com/mycompany/maven/test/SpringTest.java
@@ -5,6 +5,7 @@
  */
 package com.mycompany.maven.test;
 
+import com.mycompany.maven.spring.Message;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -15,7 +16,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class SpringTest {
     
     public static void main(String[] args) {
-        ApplicationContext appContext= new ClassPathXmlApplicationContext("");
+        ApplicationContext appContext= new ClassPathXmlApplicationContext("classpath:/beans.xml");
+        Message message = (Message) appContext.getBean("messageId");
+        System.out.println("The message is: " + message.getMessage());
     }
     
 }
