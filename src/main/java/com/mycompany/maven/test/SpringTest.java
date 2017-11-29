241754b8dd699f5b9a5e88ec4cc841ba03faa830
fjcorona <fjavier.corona@hotmail.com>
2017-11-29 12:12:24 -0600

    spring test

--- src/main/java/com/mycompany/maven/test/SpringTest.java
+++ src/main/java/com/mycompany/maven/test/SpringTest.java
@@ -6,12 +6,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringTest {
 
-    public static void main(String[] args) {
+    public static void testMessage() {
         ApplicationContext appContext = new ClassPathXmlApplicationContext("classpath:/beans.xml");
         Message message = (Message) appContext.getBean("messageId");
         System.out.println("The message is: " + message.getMessage());
         Message message2 = (Message) appContext.getBean("messageId2");
         System.out.println("The message 2 is: " + message2.getMessage());
+    }
+
+    public static void main(String[] args) {
+        testMessage();
     }
 
 }
